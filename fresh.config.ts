25003d80526a8b64267729a67d68e965be094174
Stherzada <sther@carcara.tech>
2025-04-23 12:57:43 -0300

    feat: add mcpserver

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "$fresh/server.ts";
 import { plugins } from "deco/plugins/deco.ts";
 import manifest from "./manifest.gen.ts";
+import { mcpServer } from "@deco/mcp";
+
 
 export default defineConfig({
   plugins: plugins({
     manifest,
     // deno-lint-ignore no-explicit-any
-    
+    htmx: true,
+    useServer: (deco, hono) => {
+      // @ts-ignore i know what i'm doing
+      hono.use("/*", mcpServer(deco));
+    },
   }),
 });
